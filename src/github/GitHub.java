23e918d0d93784c60099d077cbea47abe938a238
wnh1@students.ptcollege.edu <>
2018-10-22 15:25:27 -0400

    This is the first comment.

---
+++ src/github/GitHub.java
@@ -0,0 +1,19 @@
+
+package github;
+
+
+public class GitHub
+{
+
+   
+    public static void main(String[] args) 
+    {
+        Hinerman();
+    }
+    
+    public static void Hinerman()
+    {
+        System.out.println("Nathan Hinerman");
+    }
+    
+}
